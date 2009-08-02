7c06a5ec96379f6aea3d34b7ed28baa8bb0cd3fb
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2009-08-02 04:54:27 -0700

    Added RESULTS for nbody.clj-4.clj.  Created modified nbody.clj-5.clj

--- n-body/clj-run.sh
+++ n-body/clj-run.sh
@@ -9,6 +9,6 @@ JVM_MEM_OPTS="-client -Xmx1024m"
 
 #JMX_MONITORING=-Dcom.sun.management.jmxremote
 
-CLJ_PROG=nbody.clj-4.clj
+CLJ_PROG=nbody.clj-5.clj
 
 $JAVA $JVM_MEM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main $CLJ_PROG "$@"
