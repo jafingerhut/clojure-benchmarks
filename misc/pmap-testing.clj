47194d82a67f169bb706f721e2de23bc97881fd7
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2009-08-05 02:46:48 -0700

    Additional tweaks to pmap-testing.clj, and update RESULTS for fannkuch.clj-9.clj

--- misc/pmap-testing.clj
+++ misc/pmap-testing.clj
@@ -28,20 +28,20 @@
 (when (not (re-matches #"^\d+$" (nth *command-line-args* 1)))
   (usage 1))
 (def job-size
-     (let [temp (. Integer valueOf (nth *command-line-args* 1) 10)]
-       (if (== temp 0)
-         *default-repetitions*
-         temp)))
+     (let [temp (BigInteger. (nth *command-line-args* 1))]
+       (cond (not= temp (inc temp)) (do
+                                      (println (str "job-size " (nth *command-line-args* 1) " is too big to fit in Java int, so it won't work for Clojure dotimes"))
+                                      (usage 1))
+             (== temp 0) *default-repetitions*
+             :else temp)))
 
 (when (not (re-matches #"^\d+$" (nth *command-line-args* 2)))
   (usage 1))
-(def *modified-pmap-num-threads*
+(def num-threads
      (let [temp (. Integer valueOf (nth *command-line-args* 2) 10)]
-;;       (if (== temp 0)
-;;         *default-modified-pmap-num-threads*
-         temp
-;;         )
-     ))
+       (if (== temp 0)
+         *default-modified-pmap-num-threads*
+         temp)))
 
 
 (defn my-lazy-map
@@ -51,93 +51,84 @@
       (cons (f (first s)) (my-lazy-map f (rest s))))))
 
 
-;;(defn my-drop
-;;  "Returns a lazy sequence of all but the first n items in coll."
-;;  [n coll]
-;;  (let [step (fn [n coll]
-;;               (if (pos? n)
-;;                 (let [s (seq coll)]
-;;                   (if s
-;;                     (recur (dec n) (rest s))
-;;                     s))
-;;                 coll))]
-;;    (lazy-seq (step n coll))))
-
-
-;; This does what I thought pmap should do -- run about n instances of
-;; the function f at a time in parallel in separate threads, and only
-;; when one of those is complete should another instance be started.
-;; The only thing I've changed is map -> my-lazy-map.  It seems that
-;; the difference is that the version of map used by pmap optimizes
-;; for chunked collections, so if coll is chunked, it will "rush"
-;; ahead and start evaluating a whole chunk's worth of (future (f %))
-;; calls at a time.
+;; modified-pmap does what I thought pmap should do -- run about n
+;; instances of the function f at a time in parallel in separate
+;; threads, and only when one of those is complete should another
+;; instance be started.  I've changed map to my-lazy-map.  It seems
+;; that the difference is that the version of map used by pmap
+;; optimizes for chunked collections, so if coll is chunked, it will
+;; "rush" ahead and start evaluating a whole chunk's worth of (future
+;; (f %)) calls at a time.
 
 ;; Note: The number of threads run in parallel turns out to be n+1.
 ;; I'm still not sure why this is so.  In my tests, even n=0 causes 2
-;; threads to be run in parallel, so use map if you want sequential,
-;; and this version of pmap only if you want at least 2 parallel
-;; threads running most of the time.
+;; threads to be run in parallel, so we use the regular sequential map
+;; if the num-threads parameter is 1.
 
-(defn modified-pmap2
-  "Like map, except f is applied in parallel. Semi-lazy in that the
-  parallel computation stays ahead of the consumption, but doesn't
-  realize the entire result unless required. Only useful for
-  computationally intensive functions where the time of f dominates
-  the coordination overhead."
-  ([f coll]
-   (let [n (if (>= *modified-pmap-num-threads* 2)
-             (dec *modified-pmap-num-threads*)
-             1)
-         rets (my-lazy-map #(future (f %)) coll)
-         step (fn step [[x & xs :as vs] fs]
-                (lazy-seq
-                 (if-let [s (seq fs)]
-                   (cons (deref x) (step xs (rest s)))
-                   (map deref vs))))]
-     (step rets (drop n rets))))
-  ([f coll & colls]
-   (let [step (fn step [cs]
-                (lazy-seq
-                 (let [ss (my-lazy-map seq cs)]
-                   (when (every? identity ss)
-                     (cons (my-lazy-map first ss) (step (my-lazy-map rest ss)))))))]
-     (pmap #(apply f %) (step (cons coll colls))))))
+(defn modified-pmap
+  "Like pmap from Clojure 1.1, but with only as much parallelism as
+  there are available processors.  Uses my-lazy-map instead of map
+  from core.clj, since that version of map can use unwanted additional
+  parallelism for chunked collections, like ranges."
+  ([num-threads f coll]
+     (if (== num-threads 1)
+       (map f coll)
+       (let [n (if (>= num-threads 2) (dec num-threads) 1)
+             rets (my-lazy-map #(future (f %)) coll)
+             step (fn step [[x & xs :as vs] fs]
+                    (lazy-seq
+                      (if-let [s (seq fs)]
+                        (cons (deref x) (step xs (rest s)))
+                        (map deref vs))))]
+         (step rets (drop n rets)))))
+  ([num-threads f coll & colls]
+     (let [step (fn step [cs]
+                  (lazy-seq
+                    (let [ss (my-lazy-map seq cs)]
+                      (when (every? identity ss)
+                        (cons (my-lazy-map first ss) (step (my-lazy-map rest ss)))))))]
+       (modified-pmap num-threads #(apply f %) (step (cons coll colls))))))
 
 
 (defn spin-int [x]
-  (println (str "spin-int begin " x))
+;  (println (str "spin-int begin " x))
   (let [reps job-size]
+    (println (str "spin-int begin reps=" reps))
     (dotimes [_ reps]
       (inc 0)))
-  (println (str "spin-int end " x)))
+;  (println (str "spin-int end " x))
+  )
+
 
 (defn spin-double [x]
-  (println (str "spin-double begin " x))
+;  (println (str "spin-double begin " x))
   (let [reps job-size]
     (dotimes [_ reps]
 ;;      (inc (double 0.1))
       (inc 0.1)
       ))
-  (println (str "spin-double end " x)))
+;  (println (str "spin-double end " x))
+  )
 
-(defn maptest [n mapper fn]
-  (doall (mapper fn (range n))))
+
+(defn maptest [n mapper fn & num-threads]
+  (if (= mapper modified-pmap)
+    (doall (mapper (first num-threads) fn (range n)))
+    (doall (mapper fn (range n)))))
 
 
 (let [p (.. Runtime getRuntime availableProcessors)]
   (println (str "availableProcessors=" p "  parallelism used by pmap=" (+ 2 p))))
-(println (str "parallelism used by modified-pmap2=" *modified-pmap-num-threads*))
+(println (str "parallelism requested for modified-pmap=" num-threads
+              " threads"))
 (println)
 
-(println (str "(maptest " num-jobs " modified-pmap2 spin-int):"))
-(time (maptest num-jobs modified-pmap2 spin-int))
-;(println (str "(maptest " num-jobs " modified-pmap1 spin-int):"))
-;(time (maptest num-jobs modified-pmap1 spin-int))
-(println (str "(maptest " num-jobs " map spin-int):"))
-(time (maptest num-jobs map spin-int))
-(println (str "(maptest " num-jobs " pmap spin-int):"))
-(time (maptest num-jobs pmap spin-int))
+(println (str "(maptest " num-jobs " modified-pmap spin-int):"))
+(time (maptest num-jobs modified-pmap spin-int num-threads))
+;;(println (str "(maptest " num-jobs " map spin-int):"))
+;;(time (maptest num-jobs map spin-int))
+;;(println (str "(maptest " num-jobs " pmap spin-int):"))
+;;(time (maptest num-jobs pmap spin-int))
 
 ;(println (str "(maptest " num-jobs " spin-double):"))
 ;(time (maptest num-jobs spin-double))
