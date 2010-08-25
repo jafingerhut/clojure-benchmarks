0b178156c07f483107c8fd0f41799712ec8b563d
John Fingerhut <andy@our-imac.local>
2010-08-25 05:37:55 -0700

    Commit updated clj-run.sh script, although in the end it does the same thing it used to before this commit.

--- n-body/clj-run.sh
+++ n-body/clj-run.sh
@@ -5,12 +5,14 @@
 
 source ../env.sh
 
-#JVM_MEM_OPTS="-client -Xmx1024m"
-JVM_MEM_OPTS="-server"
+#JVM_OPTS="-client -Xmx1024m"
+JVM_OPTS="-server"
+#JVM_OPTS="-server -XX:+DoEscapeAnalysis -XX:+UseBiasedLocking"
 
 #JMX_MONITORING=-Dcom.sun.management.jmxremote
 
 # Current fastest
 CLJ_PROG=nbody.clj-11.clj
 
-$JAVA $JVM_MEM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main $CLJ_PROG "$@"
+1>&2 echo "$JAVA $JVM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main $CLJ_PROG" "$@"
+           $JAVA $JVM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main $CLJ_PROG "$@"
