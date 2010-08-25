1bd3794cb3759a4ebe5e30fce6bbfe3c63b1a09d
John Fingerhut <andy@our-imac.local>
2010-08-25 05:34:51 -0700

    Update to Java command line args used to run the Clojure benchmark.

--- mandelbrot/clj-run.sh
+++ mandelbrot/clj-run.sh
@@ -4,4 +4,14 @@
 
 source ../env.sh
 
-$JAVA -server -Xmx768m ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main mandelbrot.clj-3.clj "$@"
+JVM_OPTS="-server -Xmx768m"
+
+# The latest Mac OS X Java release apparently disables escape
+# analysis, and also prints a warning to the output about it.
+# Aug 25, 2010   Mac OS X 10.6.4
+# % java -version
+# java version "1.6.0_20"
+#JVM_OPTS="-server -XX:+DoEscapeAnalysis -XX:+UseBiasedLocking"
+JVM_OPTS="-server -XX:+UseBiasedLocking"
+
+$JAVA ${JVM_OPTS} ${JAVA_PROFILING} -cp ${CLOJURE_CLASSPATH} clojure.main mandelbrot.clj-5.clj "$@"
