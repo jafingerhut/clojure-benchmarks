b034785fd77b17bf51fdede2b19e7b485e20c6bf
John Fingerhut <andy@our-imac.local>
2010-08-27 02:11:19 -0700

    Clean up reverse complement's Clojure run script.

--- rcomp/clj-run.sh
+++ rcomp/clj-run.sh
@@ -5,10 +5,9 @@
 
 source ../env.sh
 
-JVM_MEM_OPTS="-client -Xmx1024m"
+#JVM_OPTS="-client -Xmx1024m"
+JVM_OPTS="-server -Xmx2048m"
 #JMX_MONITORING=-Dcom.sun.management.jmxremote
-
-CLJ_PROG=revcomp.clj-8.clj
 
 1>&2 echo "$JAVA $JVM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -classpath ${CLOJURE_CLASSPATH}:./obj/clj revcomp" "$@"
            $JAVA $JVM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -classpath ${CLOJURE_CLASSPATH}:./obj/clj revcomp "$@"
