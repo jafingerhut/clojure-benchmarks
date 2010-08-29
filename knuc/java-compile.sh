79ee53eb7cac6b409fe9b4fb97c2a16de1b9dfab
John Fingerhut <andy@our-imac.local>
2010-08-28 17:30:33 -0700

    Add faster Java program for k-nucleotide benchmark, obtained from shootout web site in August 2010.

--- knuc/java-compile.sh
+++ knuc/java-compile.sh
@@ -4,8 +4,7 @@ source ../env.sh
 
 $JAVA -version
 $JAVAC -version
-#$CP knucleotide.java-3.java knucleotide.java
-$CP knucleotide.andys.java knucleotide.java
+$CP knucleotide.java-2.java knucleotide.java
 mkdir -p obj/java
 $JAVAC -d obj/java knucleotide.java
 $RM knucleotide.java
