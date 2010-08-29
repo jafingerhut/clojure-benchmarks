c45cad5d89833b79746dde5b669703d9707e5415
John Fingerhut <andy@our-imac.local>
2010-08-28 17:31:30 -0700

    Add faster Java program for regex-dna benchmark, obtained from shootout web site in August 2010.

--- regex-dna/java-compile.sh
+++ regex-dna/java-compile.sh
@@ -4,7 +4,7 @@ source ../env.sh
 
 $JAVA -version
 $JAVAC -version
-$CP regexdna.java-2.java regexdna.java
+$CP regexdna.java-5.java regexdna.java
 mkdir -p obj/java
 $JAVAC -d obj/java regexdna.java
 $RM regexdna.java
