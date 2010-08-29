a418e4886b270d95345f28033e5dc38f7e7f1aef
John Fingerhut <andy@our-imac.local>
2010-08-28 17:29:12 -0700

    Add faster Haskell program for revcomp benchmark, copied from shootout web site in August 2010.

--- rcomp/ghc-compile.sh
+++ rcomp/ghc-compile.sh
@@ -3,6 +3,6 @@
 source ../env.sh
 
 mkdir -p obj/ghc
-$CP revcomp.ghc-2.ghc obj/ghc/revcomp.ghc-2.hs
+$CP revcomp.ghc-3.ghc obj/ghc/revcomp.ghc-3.hs
 cd obj/ghc
-$GHC --make -O2 -fglasgow-exts -fvia-C -optc-O3 revcomp.ghc-2.hs -o revcomp.ghc-2.ghc_run
+$GHC --make -O2 -fglasgow-exts -funfolding-use-threshold=32 -optc-O3 revcomp.ghc-3.hs -o revcomp.ghc-3.ghc_run
--- rcomp/ghc-run.sh
+++ rcomp/ghc-run.sh
@@ -2,4 +2,4 @@
 
 source ../env.sh
 
-./obj/ghc/revcomp.ghc-2.ghc_run 0
+./obj/ghc/revcomp.ghc-3.ghc_run 0
