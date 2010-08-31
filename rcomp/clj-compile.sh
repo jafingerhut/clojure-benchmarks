02462f9bafd292fddf0964231c98f307edaee8e9
John Fingerhut <andy@our-imac.local>
2010-08-31 04:29:51 -0700

    Added revcomp.clj-10.clj, which runs the long benchmark in about half the time of revcomp.clj-9.clj on my iMac.  It is based on revcomp.java-4.java from the shootout web site, except it uses a StringBuilder instead of a class that extends ByteArrayOutputStream. I would have liked to try something even closer to the revcomp.java-4.java approach, but I do not know how to extend ByteArrayOutputStream and access protected members like count and buf.

--- rcomp/clj-compile.sh
+++ rcomp/clj-compile.sh
@@ -3,6 +3,6 @@
 source ../env.sh
 mkdir -p ./obj/clj
 
-cp revcomp.clj-9.clj ./obj/clj/revcomp.clj
+cp revcomp.clj-10.clj ./obj/clj/revcomp.clj
 
 java -Dclojure.compile.path=./obj/clj -classpath ${CLOJURE_CLASSPATH}:./obj/clj clojure.lang.Compile revcomp
--- rcomp/clj-run.sh
+++ rcomp/clj-run.sh
@@ -6,7 +6,7 @@
 source ../env.sh
 
 #JVM_OPTS="-client -Xmx1024m"
-JVM_OPTS="-server -Xmx2048m"
+JVM_OPTS="-server -Xmx1536m"
 #JMX_MONITORING=-Dcom.sun.management.jmxremote
 
 1>&2 echo "$JAVA $JVM_OPTS $JMX_MONITORING ${JAVA_PROFILING} -classpath ${CLOJURE_CLASSPATH}:./obj/clj revcomp" "$@"
