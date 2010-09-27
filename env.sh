f7ed67ed2b09880fdc405d3fee8a1c6f5c2f9e50
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-09-27 00:39:43 -0700

    Correct mistakes in Windows+Cygwin part of env.sh

--- env.sh
+++ env.sh
@@ -45,23 +45,23 @@ then
     if [ "${CLJ_VERSION:=clj-1.2.0}" == "clj-1.2.0" ]
     then
         # Let default Clojure version be 1.2.0 if none is specified.
-        CLOJURE_JAR_DIR="${HOME_DIR}\lein\swank-clj-1.2.0\lib"
-        CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\clojure-1.2.0.jar:${CLOJURE_JAR_DIR}\clojure-contrib-1.2.0.jar"
+        CLOJURE_JAR_DIR="${HOME_DIR}\\lein\\swank-clj-1.2.0\\lib"
+        CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\\clojure-1.2.0.jar;${CLOJURE_JAR_DIR}\\clojure-contrib-1.2.0.jar"
     elif [ "$CLJ_VERSION" == "clj-1.3.0-alpha1" ]
     then
-        CLOJURE_JAR_DIR="${HOME_DIR}\lein\clj-1.3.0-alpha1\lib"
-        CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\clojure-1.3.0-alpha1.jar"
-        #CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\clojure-1.3.0-alpha1.jar:${CLOJURE_JAR_DIR}\complete-1.3.1-20100923.180543-12-bin.jar"
+        CLOJURE_JAR_DIR="${HOME_DIR}\\lein\\clj-1.3.0-alpha1\\lib"
+        CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\\clojure-1.3.0-alpha1.jar"
+        #CLOJURE_CLASSPATH="${CLOJURE_JAR_DIR}\\clojure-1.3.0-alpha1.jar;${CLOJURE_JAR_DIR}\\complete-1.3.1-20100923.180543-12-bin.jar"
     else
         1>&2 echo "$0: CLJ_VERSION='${CLJ_VERSION}' must be one of: clj-1.2.0 clj-1.3.0-alpha1"
         exit 1
     fi
 
     # Platform-specific form of Clojure object file directory
-    PS_CLJ_OBJ_DIR=".${SEP}obj${SEP}clj"
+    PS_CLJ_OBJ_DIR=".${SEP}obj${SEP}${CLJ_VERSION}"
     PS_FULL_CLJ_CLASSPATH="${CLOJURE_CLASSPATH}${PSEP}${PS_CLJ_OBJ_DIR}"
     # Unix form, still useful in some places on Cygwin
-    CLJ_OBJ_DIR="./obj/clj"
+    CLJ_OBJ_DIR="./obj/${CLJ_VERSION}"
 
     ######################################################################
     # Windows+Cygwin SBCL
