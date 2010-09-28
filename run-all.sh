dba6152720c2272596c8ada1fe69c1151450624c
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-09-27 22:46:07 -0700

    Now that n-body is reasonably fast with Clojure 1.3 alpha1, add it back into run-all.sh's list of benchmarks to run.

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-#for j in fannkuch fannkuchredux fasta knuc mandelbrot n-body rcomp regex-dna
-
-for j in fannkuch fannkuchredux fasta knuc mandelbrot rcomp regex-dna
+for j in fannkuch fannkuchredux fasta knuc mandelbrot n-body rcomp regex-dna
 do
     cd $j
     ./batch.sh "$@"
