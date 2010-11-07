9894ead210309560dd0057e2bf111a80c571976f
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-07 12:30:17 -0800

    Add much more info to output of run-one.pl, more command line options for controlling it, and better help on usage.

--- bin/run-one.pl
+++ bin/run-one.pl
@@ -3,33 +3,6 @@ eval 'exec perl -S "$0" ${1+"$@"}'
     if 0;
 # -*cperl-*-
 
-######################################################################
-# Examples of use:
-
-######################################################################
-# Windows XP SP3 + Cygwin:
-######################################################################
-
-# Admin@john-win ~/git/clojure-benchmarks/knuc
-# % ../bin/run-one.pl -i input\\medium-input.txt -o output\\medium-clj-1.2-output.txt \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -version
-# 0.44,0.36,0.09,17080
-
-######################################################################
-# Mac OS X 10.5.8 (and probably also 10.6.x)
-######################################################################
-
-# andy@andys-mbp /Users/Shared/lang/clojure-benchmarks/knuc
-# % ../bin/run-one.pl -i input/medium-input.txt -o output/medium-clj-1.2-output.txt java -version
-# 0.18,0.09,0.03,13688
-
-######################################################################
-# Ubuntu 10.4 LTS with GNU time 1.7
-######################################################################
-
-# andy@andys-mbp ~/sw/git/clojure-benchmarks/knuc
-# % ../bin/run-one.pl -i input/medium-input.txt -o output/medium-clj-1.2-output.txt java -version
-
-
 use strict;
 use Getopt::Std;
 use File::Basename;
@@ -52,40 +25,60 @@ use Symbol qw(gensym);
 use IO::File;
 
 
-my $debug = 0;
+my $verbose = 0;
 
 my $full_progname = $0;
 my $progname = fileparse($full_progname);
 
 
-my $os;
-open(F,"uname -o|") or die sprintf "Could not run command 'uname -o' to determine operating system.  Aborting.\n";
-$os = <F>;
+my $os = `uname -o`;
 chomp $os;
-close(F);
-if ($debug) {
+if ($verbose) {
     printf STDERR "\$os='%s'\n", $os;
 }
+my $os_full = `uname -a`;
+chomp $os_full;
 
 
 sub usage {
     print STDERR "usage: $progname [-h] [ file1 ... ]\n";
     print STDERR "
+    -h            Show this help.
+    -n            Print a 'header line' giving the name of each field
+                  in the output CSV file, in addition to the
+                  statistics line.
+    -v            Enable debug messages.
+    -s <os_name>  Can be used to specify one of the OS's: Cygwin,
+                  Darwin, GNU/Linux
+    -t <time_cmd> to specify the command to use to measure the running
+                  time and memory usage of the process.
+    -i <input_file>
+    -o <output_file>
+    -l <language_implementation_description_string>
+    -b <benchmark_problem_name>
 
-description here
+Example of use on Linux or Mac OS X:
+
+  % ../bin/run-one.pl -i input/medium-input.txt -o output/medium-clj-1.2-output.txt java -version
+  0.18,0.09,0.03,13688
+
+Example of use on Windows XP + Cygwin:
+
+  % ../bin/run-one.pl -i input\\medium-input.txt -o output\\medium-clj-1.2-output.txt \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -version
+  0.44,0.36,0.09,17080
 ";
 }
 
 my $opts = { };
-getopts('hnds:t:i:o:', $opts);
+getopts('hnvs:t:i:o:l:b:', $opts);
 
 if ($opts->{h}) {
     usage();
     exit(0);
 }
 
-if ($opts->{d}) {
-    $debug = 1;
+if ($opts->{v}) {
+    $verbose = 1;
 }
 if ($opts->{s}) {
     $os = $opts->{s};
@@ -101,6 +94,9 @@ if ($os eq 'Cygwin') {
     $timemem_cmd = "/usr/bin/time -lp";
 } elsif ($os eq 'GNU/Linux') {
     $timemem_cmd = "/usr/bin/time -v";
+} else {
+    printf STDERR "Unknown OS string '%s'.  Only 'Cygwin', 'Darwin', and 'GNU/Linux' are supported.  Aborting.";
+    exit 1;
 }
 
 if ($opts->{t}) {
@@ -114,9 +110,24 @@ my $output_file;
 if ($opts->{o}) {
     $output_file = $opts->{o};
 }
+my $language_implementation_desc_str = '';
+if ($opts->{l}) {
+    $language_implementation_desc_str = $opts->{l};
+}
+my $benchmark_name = '';
+if ($opts->{b}) {
+    $benchmark_name = $opts->{b};
+}
+my $source_file_name = '';
 
+
+if ($#ARGV < 0) {
+    printf STDERR "No command given.\n";
+    usage();
+    exit 1;
+}
 my $cmd_to_time = join(' ', @ARGV);
-if ($debug) {
+if ($verbose) {
     printf STDERR "\$cmd_to_time='%s'\n", $cmd_to_time;
 }
 
@@ -135,7 +146,7 @@ if (defined($input_file)) {
 if (defined($output_file)) {
     $cmd_str .= sprintf " > %s", $output_file;
 }
-if ($debug) {
+if ($verbose) {
     printf STDERR "\$cmd_str='%s'\n", $cmd_str;
 }
 
@@ -145,7 +156,7 @@ if ($os eq 'Cygwin') {
     #my $tempdir = tempdir( CLEANUP => 1 );
     my $tempdir = tempdir( CLEANUP => 0 );
     my ($fh, $batchfile) = tempfile( DIR => $tempdir, SUFFIX => '.bat' );
-    if ($debug) {
+    if ($verbose) {
 	printf STDERR "\$tempdir='%s'\n", $tempdir;
     }
     printf $fh "%s\n", $cmd_str;
@@ -155,7 +166,7 @@ if ($os eq 'Cygwin') {
 } else {
     $cmd_to_run = $cmd_str;
 }
-if ($debug) {
+if ($verbose) {
     printf STDERR "\$cmd_to_run='%s'\n", $cmd_to_run;
 }
 
@@ -169,10 +180,12 @@ my ($elapsed_sec, $user_sec, $sys_sec, $max_rss_kbytes);
 # http://perldoc.perl.org/perlfaq8.html#How-can-I-capture-STDERR-from-an-external-command?
 
 local *CATCHERR = IO::File->new_tmpfile;
+my $start_time = localtime();
 my $pid = open3(gensym, \*CATCHOUT, ">&CATCHERR", $cmd_to_run);
 while (<CATCHOUT>) {
 }
 waitpid($pid, 0);
+my $end_time = localtime();
 seek CATCHERR, 0, 0;
 
 #open(F,$cmd_to_run . "|") or die sprintf "Could not run command '%s'.  Aborting.\n", $cmd_to_run;
@@ -264,16 +277,47 @@ if ($os eq 'Cygwin') {
     }
 }
 close(F);
-if ($debug) {
+if ($verbose) {
     printf STDERR "\$elapsed_sec='%s'\n", $elapsed_sec;
     printf STDERR "\$user_sec='%s'\n", $user_sec;
     printf STDERR "\$sys_sec='%s'\n", $sys_sec;
     printf STDERR "\$max_rss_kbytes='%s'\n", $max_rss_kbytes;
 }
 if ($opts->{n}) {
-    printf "elapsed_sec,user_sec,sys_sec,max_rss_kbytes\n";
+    printf "\"OS description\",\"Language implementation\",\"Benchmark name\",\"Source file name\",\"Start time\",\"End time\",\"Command measured\",elapsed_sec,user_sec,sys_sec,max_rss_kbytes\n";
 }
+printf "%s,", csv_str($os_full);
+printf "%s,", csv_str($language_implementation_desc_str);
+printf "%s,", csv_str($benchmark_name);
+printf "%s,", csv_str($source_file_name);
+printf "%s,", csv_str($start_time);
+printf "%s,", csv_str($end_time);
+printf "%s,", csv_str($cmd_to_time);
 printf "%s,%s,%s,%s", $elapsed_sec, $user_sec, $sys_sec, $max_rss_kbytes;
+# TBD: percent busy time for each cpu core
+# TBD: percent of cpu this job got while it ran
+# TBD: exit status
+#    description string for result summary:
+#        normal completion, output correct
+#        normal completion, output incorrect
+#	completed with exception <name>
+#        unknown type of completion, output incorrect
+#        killed after taking too long
+#    tbd: some measure of the maximum memory ever used by all
+#        simultaneously live Java objects, in kbytes (tbd: check out
+#        -verbose:gc output, and learn how to interpret the values to
+#        see if they can provide this measurement)
+
 printf "\n";
 
 exit 0;
+
+
+sub csv_str {
+    my $str = shift;
+    my $csv_str;
+
+    $csv_str = $str;
+    $csv_str =~ s/"/""/g;
+    return '"' . $csv_str . '"';
+}
