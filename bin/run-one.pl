9fe13f5ac7d6132ed9796cc6827e9f3e5065fe04
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-07 20:16:05 -0800

    Add printing of the exit status of the measured command, and a command line option to choose between a human-readable output format, or CSV.

--- bin/run-one.pl
+++ bin/run-one.pl
@@ -14,12 +14,6 @@ use File::Temp qw/ tempfile tempdir /;
 # someone installs 'the whole package' of files, the script can find
 # them, without needing hand-editing.
 use FindBin;
-# An example use of the $FindBin::Bin string variable defined by the
-# line above, to include the 'build' subdirectory beneath that in the
-# list of directories searched when finding .pm files for later 'use'
-# statements.  Useful if a .pm file is part of 'the whole package' you
-# want to distribute with this script.
-use lib "$FindBin::Bin" . "/build";
 use IPC::Open3;
 use Symbol qw(gensym);
 use IO::File;
@@ -33,9 +27,6 @@ my $progname = fileparse($full_progname);
 
 my $os = `uname -o`;
 chomp $os;
-if ($verbose) {
-    printf STDERR "\$os='%s'\n", $os;
-}
 my $os_full = `uname -a`;
 chomp $os_full;
 
@@ -44,10 +35,12 @@ sub usage {
     print STDERR "usage: $progname [-h] [ file1 ... ]\n";
     print STDERR "
     -h            Show this help.
-    -n            Print a 'header line' giving the name of each field
-                  in the output CSV file, in addition to the
-                  statistics line.
     -v            Enable debug messages.
+    -c            Print output in CSV format, not really intended for
+                  human consumption.
+    -n            If -c is given, then also print a 'header line'
+                  giving the name of each field in the output CSV
+                  file, in addition to the statistics line.
     -s <os_name>  Can be used to specify one of the OS's: Cygwin,
                   Darwin, GNU/Linux
     -t <time_cmd> to specify the command to use to measure the running
@@ -60,17 +53,31 @@ sub usage {
 Example of use on Linux or Mac OS X:
 
   % ../bin/run-one.pl -i input/medium-input.txt -o output/medium-clj-1.2-output.txt java -version
-  0.18,0.09,0.03,13688
+  [ ... output removed ... ]
 
-Example of use on Windows XP + Cygwin:
+  % ../bin/run-one.pl -i input/medium-input.txt -o output/medium-clj-1.2-output.txt java -server -Xmx1536m -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 knucleotide
+      Command measured          : java -server -Xmx1536m -classpath /Users/andy/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 knucleotide
+      Elapsed time (sec)        : 82.35
+      User CPU time (sec)       : 26.34
+      System CPU time (sec)     : 0.65
+      Max resident set size (kb): 0
+      Start time                : Sun Nov  7 20:01:20 2010
+      End time                  : Sun Nov  7 20:02:42 2010
+      Exit status               : 0
+      OS description            : Darwin our-imac.local 10.4.0 Darwin Kernel Version 10.4.0: Fri Apr 23 18:28:53 PDT 2010; root:xnu-1504.7.4~1/RELEASE_I386 i386 i386 iMac6,1 Darwin
+
+Examples of use on Windows XP + Cygwin:
 
   % ../bin/run-one.pl -i input\\medium-input.txt -o output\\medium-clj-1.2-output.txt \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -version
-  0.44,0.36,0.09,17080
+  [ ... output removed ... ]
+
+  % ../bin/run-one.pl -v -i input\\quick-input.txt -o output\\quick-clj-1.2-output.txt \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx1536m -classpath \"\\cygwin\\home\\Admin\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2\" knucleotide
+  [ ... output removed ... ]
 ";
 }
 
 my $opts = { };
-getopts('hnvs:t:i:o:l:b:', $opts);
+getopts('hvcns:t:i:o:l:b:', $opts);
 
 if ($opts->{h}) {
     usage();
@@ -82,6 +89,10 @@ if ($opts->{v}) {
 }
 if ($opts->{s}) {
     $os = $opts->{s};
+}
+if ($verbose) {
+    printf STDERR "\$os='%s'\n", $os;
+    printf STDERR "install dir='%s'\n", $FindBin::Bin;
 }
 
 # TBD: The default path to timemem should be based on full path name,
@@ -153,8 +164,8 @@ if ($verbose) {
 my $cmd_to_run;
 if ($os eq 'Cygwin') {
     # On Cygwin, put the command into a BAT file for execution.
-    #my $tempdir = tempdir( CLEANUP => 1 );
-    my $tempdir = tempdir( CLEANUP => 0 );
+    my $tempdir = tempdir( CLEANUP => 1 );
+    #my $tempdir = tempdir( CLEANUP => 0 );
     my ($fh, $batchfile) = tempfile( DIR => $tempdir, SUFFIX => '.bat' );
     if ($verbose) {
 	printf STDERR "\$tempdir='%s'\n", $tempdir;
@@ -185,11 +196,13 @@ my $pid = open3(gensym, \*CATCHOUT, ">&CATCHERR", $cmd_to_run);
 while (<CATCHOUT>) {
 }
 waitpid($pid, 0);
+my $child_exit_status = $? >> 8;
 my $end_time = localtime();
 seek CATCHERR, 0, 0;
 
 #open(F,$cmd_to_run . "|") or die sprintf "Could not run command '%s'.  Aborting.\n", $cmd_to_run;
 
+my $cmd_exit_status;
 if ($os eq 'Cygwin') {
     my $found_pid = 0;
     while (<CATCHERR>) {
@@ -198,7 +211,9 @@ if ($os eq 'Cygwin') {
 	if (/^Process ID/) {
 	    $found_pid = 1;
 	} elsif ($found_pid) {
-	    if (/^\s+elapsed time \(seconds\): (.*)\s*$/) {
+	    if (/^\s+exit code: (.*)\s*$/) {
+		$cmd_exit_status = $1;
+	    } elsif (/^\s+elapsed time \(seconds\): (.*)\s*$/) {
 		$elapsed_sec = $1;
 	    } elsif (/^\s+user time \(seconds\): (.*)\s*$/) {
 		$user_sec = $1;
@@ -216,6 +231,7 @@ if ($os eq 'Cygwin') {
 	}
     }
 } elsif ($os eq 'Darwin') {
+    $cmd_exit_status = $child_exit_status;
     my $found_real = 0;
     while (<CATCHERR>) {
 	chomp;
@@ -228,6 +244,8 @@ if ($os eq 'Cygwin') {
 	    } elsif (/^\s*sys\s+(.*)\s*$/) {
 		$sys_sec = $1;
 	    } elsif (/^\s+(\d+)\s+maximum resident set size\s*$/) {
+		# Mac OS X reports max RSS in units of bytes.  Convert
+		# to kbytes.
 		$max_rss_kbytes = $1 / 1024;
 	    } else {
 		# Ignore the others
@@ -239,6 +257,7 @@ if ($os eq 'Cygwin') {
 	}
     }
 } elsif ($os eq 'GNU/Linux') {
+    $cmd_exit_status = $child_exit_status;
     my $found_user = 0;
     while (<CATCHERR>) {
 	chomp;
@@ -260,11 +279,22 @@ if ($os eq 'Cygwin') {
                     die sprintf "Unrecognized Elapsed (wall clock) time format seen.  Aborting:\n%s", $tmp;
                 }
 	    } elsif (/^\s*Maximum resident set size \(kbytes\):\s*(\d+)\s*$/) {
-                # Note: There is a bug in GNU time version 1.7 (see
-                # which version you have using the command
-                # '/usr/bin/time -V') which causes the maximum
-                # resident set size to be 4 times too large.  See:
+                # There is a bug in GNU time version 1.7 that causes
+                # the maximum resident set size reported to be 4 times
+                # too large.  See:
+
                 # http://www.mail-archive.com/help-gnu-utils@gnu.org/msg01371.html
+		#
+		# You can find out which version of GNU time you have
+		# in Cygwin using this command:
+		#
+                #     /usr/bin/time -V
+		#
+		# If that gives an error message like 'No such file or
+		# directory', then you must use the Cygwin Setup
+		# program to add the 'time' package.  You can search
+		# for it by name, or look for it in the 'Utils'
+		# category.
 		$max_rss_kbytes = $1 / 4;
 	    } else {
 		# Ignore the others
@@ -283,35 +313,74 @@ if ($verbose) {
     printf STDERR "\$sys_sec='%s'\n", $sys_sec;
     printf STDERR "\$max_rss_kbytes='%s'\n", $max_rss_kbytes;
 }
-if ($opts->{n}) {
-    printf "\"OS description\",\"Language implementation\",\"Benchmark name\",\"Source file name\",\"Start time\",\"End time\",\"Command measured\",elapsed_sec,user_sec,sys_sec,max_rss_kbytes\n";
+if ($opts->{c}) {
+    if ($opts->{n}) {
+	# TBD: Update this to match the order below
+	printf "\"OS description\",\"Language implementation\",\"Benchmark name\",\"Source file name\",\"Start time\",\"End time\",\"Command measured\",elapsed_sec,user_sec,sys_sec,max_rss_kbytes\n";
+    }
+    printf "%s", csv_str($benchmark_name);
+    printf ",%s", csv_str($language_implementation_desc_str);
+    printf ",%s", csv_str($source_file_name);
+    printf ",%s", csv_str($cmd_to_time);
+    printf ",%s", $elapsed_sec;
+    printf ",%s", $user_sec;
+    printf ",%s", $sys_sec;
+    printf ",%s", $max_rss_kbytes;
+    printf ",%s", csv_str($start_time);
+    printf ",%s", csv_str($end_time);
+    # TBD: percent busy time for each cpu core
+    # TBD: percent of cpu this job got while it ran
+    printf ",%s", $cmd_exit_status;
+    # TBD:
+    #    description string for result summary:
+    #        normal completion, output correct
+    #        normal completion, output incorrect
+    #	completed with exception <name>
+    #        unknown type of completion, output incorrect
+    #        killed after taking too long
+    #    tbd: some measure of the maximum memory ever used by all
+    #        simultaneously live Java objects, in kbytes (tbd: check out
+    #        -verbose:gc output, and learn how to interpret the values to
+    #        see if they can provide this measurement)
+    # TBD: Description of hardware, e.g. # of CPU cores, type, and
+    #     clock speed of each.
+    printf ",%s", csv_str($os_full);
+    printf "\n";
+} else {
+    if ($benchmark_name ne '') {
+	printf "    Benchmark name: %s\n", $benchmark_name;
+    }
+    if ($language_implementation_desc_str ne '') {
+	printf "    Language implementation: %s\n", $language_implementation_desc_str;
+    }
+    if ($source_file_name ne '') {
+	printf "    Source file name: %s\n", $source_file_name;
+    }
+    printf "    Command measured          : %s\n", $cmd_to_time;
+    printf "    Elapsed time (sec)        : %s\n", $elapsed_sec;
+    printf "    User CPU time (sec)       : %s\n", $user_sec;
+    printf "    System CPU time (sec)     : %s\n", $sys_sec;
+    printf "    Max resident set size (kb): %s\n", $max_rss_kbytes;
+    printf "    Start time                : %s\n", $start_time;
+    printf "    End time                  : %s\n", $end_time;
+    printf "    Exit status               : %s\n", $cmd_exit_status;
+    printf "    OS description            : %s\n", $os_full;
 }
-printf "%s,", csv_str($os_full);
-printf "%s,", csv_str($language_implementation_desc_str);
-printf "%s,", csv_str($benchmark_name);
-printf "%s,", csv_str($source_file_name);
-printf "%s,", csv_str($start_time);
-printf "%s,", csv_str($end_time);
-printf "%s,", csv_str($cmd_to_time);
-printf "%s,%s,%s,%s", $elapsed_sec, $user_sec, $sys_sec, $max_rss_kbytes;
-# TBD: percent busy time for each cpu core
-# TBD: percent of cpu this job got while it ran
-# TBD: exit status
-#    description string for result summary:
-#        normal completion, output correct
-#        normal completion, output incorrect
-#	completed with exception <name>
-#        unknown type of completion, output incorrect
-#        killed after taking too long
-#    tbd: some measure of the maximum memory ever used by all
-#        simultaneously live Java objects, in kbytes (tbd: check out
-#        -verbose:gc output, and learn how to interpret the values to
-#        see if they can provide this measurement)
-
-printf "\n";
 
 exit 0;
 
+
+# Referring to the "Specification" section of this Wikipedia page:
+
+# http://en.wikipedia.org/wiki/Comma-separated_values
+
+# especially the "Basic rules" part of that section, it appears to me
+# that I should be able to take _any_ string I want to put in a CSV
+# field, whether it has quotes, commas, semicolons, etc. or not, put
+# enclose it in double quotes, and replace all double quotes within
+# the string with two consecutive double quotes, and it should be
+# valid.  If I'm misunderstanding that, then I should be able to fix
+# it by modifying this function.
 
 sub csv_str {
     my $str = shift;
