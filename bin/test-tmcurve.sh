a39b2dacd3879a0528fbd5220dbbd271898edee5
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-20 14:51:22 -0800

    Update tmcurve arguments in test-tmcurve.sh.

--- bin/test-tmcurve.sh
+++ bin/test-tmcurve.sh
@@ -24,9 +24,12 @@ fi
 
 set -ex
 
+COMMON_ARGS="--verbose --alpha 1.05 --sorted-summary --results-file results.xml"
+#COMMON_ARGS="--verbose --alpha 1.05 --sorted-summary --results-file results.xml --sweep-only --num-mb-values 2"
+
 if [ "$OS" == "Cygwin" ]
 then
-    ../bin/tmcurve --verbose --sorted-summary --results-file results.xml --input input\\${INPUT_SIZE}-input.txt --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Administrator\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
+    ../bin/tmcurve ${COMMON_ARGS} --input input\\${INPUT_SIZE}-input.txt --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Administrator\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
 else
-    ../bin/tmcurve --verbose --sorted-summary --results-file results.xml --input input/${INPUT_SIZE}-input.txt --output output/${INPUT_SIZE}-clj-1.2-output.txt --check "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt %o" java -server -Xmx%mbm -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 ${BENCHMARK}
+    ../bin/tmcurve ${COMMON_ARGS} --input input/${INPUT_SIZE}-input.txt --output output/${INPUT_SIZE}-clj-1.2-output.txt --check "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt %o" java -server -Xmx%mbm -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 ${BENCHMARK}
 fi
