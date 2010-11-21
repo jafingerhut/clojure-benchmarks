cd8459de73c3cbebe6b45d6be12450871202a00b
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-21 07:38:11 -0800

    Make it easy to test programs with no input file to test-tmcurve.sh.

--- bin/test-tmcurve.sh
+++ bin/test-tmcurve.sh
@@ -27,9 +27,22 @@ set -ex
 COMMON_ARGS="--verbose --jvm-gc-stats --alpha 1.05 --sorted-summary --results-file results.xml"
 #COMMON_ARGS="--verbose --jvm-gc-stats --alpha 1.05 --sorted-summary --results-file results.xml --sweep-only --num-mb-values 2"
 
+# Useful when testing hello.clj memory use
+#EXTRA_ARGS="--min 1 --precision 1"
+
+if [ "${INPUT_SIZE}" == "none" ]; then
+    INFILE_ARGS=""
+else
+    if [ "$OS" == "Cygwin" ]; then
+	INFILE_ARGS="--input input\\${INPUT_SIZE}-input.txt"
+    else
+	INFILE_ARGS="--input input/${INPUT_SIZE}-input.txt"
+    fi
+fi
+
 if [ "$OS" == "Cygwin" ]
 then
-    ../bin/tmcurve ${COMMON_ARGS} --input input\\${INPUT_SIZE}-input.txt --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Administrator\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
+    ../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Administrator\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
 else
-    ../bin/tmcurve ${COMMON_ARGS} --input input/${INPUT_SIZE}-input.txt --output output/${INPUT_SIZE}-clj-1.2-output.txt --check "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt %o" java -server -Xmx%mbm -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 ${BENCHMARK}
+    ../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output/${INPUT_SIZE}-clj-1.2-output.txt --check "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt %o" java -server -Xmx%mbm -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 ${BENCHMARK}
 fi
