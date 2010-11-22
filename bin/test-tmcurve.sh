61ec8c6db4e9f6ad3ac51493b9ec04fa745251c1
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-21 17:13:46 -0800

    Add lines to test-tmcurve.sh so that it is quick to switch between invoking JRockit or HotSpot on my Windows machine.

--- bin/test-tmcurve.sh
+++ bin/test-tmcurve.sh
@@ -40,9 +40,16 @@ else
     fi
 fi
 
+JROCKIT=0
+
 if [ "$OS" == "Cygwin" ]
 then
-    ../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Administrator\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
+    if [ $JROCKIT -eq 1 ]
+    then
+	../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jrmc-4.0.1-1.6.0\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Admin\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
+    else
+	../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output\\${INPUT_SIZE}-clj-1.2-output.txt --check-output-cmd "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt output/${INPUT_SIZE}-clj-1.2-output.txt" \\Program\ Files\\Java\\jdk1.6.0_21\\bin\\java -server -Xmx%mbm -classpath "\\cygwin\\home\\Admin\\lein\\swank-clj-1.2.0\\lib\\clojure-1.2.0.jar;.\\obj\\clj-1.2" ${BENCHMARK}
+    fi
 else
     ../bin/tmcurve ${COMMON_ARGS} ${EXTRA_ARGS} ${INFILE_ARGS} --output output/${INPUT_SIZE}-clj-1.2-output.txt --check "diff --strip-trailing-cr --brief output/${INPUT_SIZE}-expected-output.txt %o" java -server -Xmx%mbm -classpath ~/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar:./obj/clj-1.2 ${BENCHMARK}
 fi
