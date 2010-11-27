53d30dfe5217560241feb608a5543aa8f4a30f64
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-27 00:11:10 -0800

    Add some command line options and printing of more system and memory details to jvm-ptr-size.clj

--- misc/jvm-ptr-size.clj
+++ misc/jvm-ptr-size.clj
@@ -1,44 +1,62 @@
-;; Show all JVM properties, but especially at the end the two that I
-;; believe have the most to do in determining whether the JVM is
-;; 32-bit or 64-bit.  In particular sun.arch.data.model seems to be
-;; the one introduced by Sun for this purpose.
-
 (comment
-(let [props (. System getProperties)
-      keys (. props keys)
-      s (enumeration-seq keys)
-      ]
-  ;(printf "(class props)=%s\n" (class props))
-  ;(printf "(class *out*)=%s\n" (class *out*))
-  ;(printf "(class keys)=%s\n" (class keys))
-  ;(printf "(class s)=%s\n" (class s))
-  ;(printf "(count s)=%s\n" (count s))
+(loop [i 0
+       a *command-line-args*]
+  (if (seq a)
+    (do
+      (printf "%d: '%s'\n" i (first a))
+      (recur (inc i) (rest a)))
+    ))
+)
+
+(def *show* (if (>= (count *command-line-args*) 1)
+              (nth *command-line-args* 0)
+              "other"))
+
+(when (= *show* "allproperties")
+  (let [props (. System getProperties)
+        keys (. props keys)
+        s (enumeration-seq keys)]
+    ;;(printf "(class props)=%s\n" (class props))
+    ;;(printf "(class *out*)=%s\n" (class *out*))
+    ;;(printf "(class keys)=%s\n" (class keys))
+    ;;(printf "(class s)=%s\n" (class s))
+    ;;(printf "(count s)=%s\n" (count s))
     (doseq [prop-name (sort s)]
       (printf "%s=%s\n" prop-name (. System getProperty prop-name)))
     (printf "\n")
     (printf "number of properties=%d\n" (count s))
-    (printf "\n"))
-)
+    (flush))
+  (. System exit 0))
+  
+(when (= *show* "sysinfo")
+  ;; Show all JVM properties, but especially at the end the two that I
+  ;; believe have the most to do in determining whether the JVM is
+  ;; 32-bit or 64-bit.  In particular sun.arch.data.model seems to be
+  ;; the one introduced by Sun for this purpose.
 
-(printf "sun.arch.data.model='%s'\n"
-        (. System getProperty "sun.arch.data.model"))
-(printf "os.arch='%s'\n"
-        (. System getProperty "os.arch"))
+  (printf "os.name = %s\n" (. System getProperty "os.name"))
+  (printf "os.version = %s\n" (. System getProperty "os.version"))
+  (printf "java.runtime.version = %s\n"
+          (. System getProperty "java.runtime.version"))
+  (printf "sun.arch.data.model = %s\n"
+          (. System getProperty "sun.arch.data.model"))
+  (printf "os.arch = %s\n" (. System getProperty "os.arch"))
+  (let [rt (. Runtime getRuntime)]
+    (printf "availableProcessors = %d\n" (. rt availableProcessors)))
+  (flush)
+  (. System exit 0))
+
 ;; Source for documentation on Runtime object:
 ;; http://download.oracle.com/javase/1.4.2/docs/api/java/lang/Runtime.html
 (let [rt (. Runtime getRuntime)
       total (. rt totalMemory)
       max (. rt maxMemory)
-      free (. rt freeMemory)]
-  (printf "Runtime.getRuntime(). ...\n")
-  (printf "   totalMemory() = %d = %.1f MB\n"
-          total (/ total (* 1024.0 1024.0)))
-  (printf "   maxMemory() = %d = %.1f MB\n"
-          max (/ max (* 1024.0 1024.0)))
-  (printf "   freeMemory() = %d bytes = %.1f MB\n"
-          free (/ free (* 1024.0 1024.0)))
-  (printf "   availableProcessors() = %d\n"
-          (. rt availableProcessors))
+      free (. rt freeMemory)
+      mb (* 1024.0 1024.0)]
+  (printf "total=%d= %.1f MB" total (/ total mb))
+  (printf "  max=%d= %.1f MB" max (/ max mb))
+  (printf "  free=%d= %.1f MB" free (/ free mb))
+  (printf "\n")
 (comment
   (printf (str
 "        totalMemory()\n"
