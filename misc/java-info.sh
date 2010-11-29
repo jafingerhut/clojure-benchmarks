c0ae8d118427a8a51106aefb21d00bbcd2b9f24c
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-28 19:48:10 -0800

    Change java-info.sh to take advantage of env.sh, rather than redoing work done in env.sh.

--- misc/java-info.sh
+++ misc/java-info.sh
@@ -1,44 +1,10 @@
 #! /bin/bash
 
-OS=`uname -o 2>/dev/null`
-if [ $? -ne 0 ]
-then
-    # Option -o does not exist in Mac OS X default version of uname
-    # command.
-    OS=`uname -s 2>/dev/null`
-fi
+source ../env.sh
 
 set -x
-
-JAVA="${1:-none-given}"
-
-if [ "$OS" == "Cygwin" ]
-then
-    if [ "$JAVA" == "none-given" ]
-    then
-	JAVA_BIN="/cygdrive/c/Program Files/Java/jrmc-4.0.1-1.6.0/bin"
-	JAVA="java"
-	echo "Got here 1"
-    fi
-    CP1=`cygpath -w ${HOME}/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar`
-    CP2=`cygpath -w ${HOME}/lein/swank-clj-1.2.0/lib/clojure-contrib-1.2.0.jar`
-    CP3="."
-    PSEP=";"
-    CLASSPATH="${CP1}${PSEP}${CP2}${PSEP}${CP3}"
-else
-    if [ "$JAVA" == "none-given" ]
-    then
-	JAVA="java"
-    fi
-    CP1="${HOME}/lein/swank-clj-1.2.0/lib/clojure-1.2.0.jar"
-    CP2="${HOME}/lein/swank-clj-1.2.0/lib/clojure-contrib-1.2.0.jar"
-    CP3="."
-    PSEP=":"
-    CLASSPATH="${CP1}${PSEP}${CP2}${PSEP}${CP3}"
-fi
-
-which java
 "${JAVA}" -version
+
 for TYPE in -server -client
 do
     set +x
@@ -48,13 +14,13 @@ do
 
     set -x
     "${JAVA}" "${TYPE}" -version
-    "${JAVA}" "${TYPE}" -cp "${CLASSPATH}" clojure.main jvm-ptr-size.clj sysinfo
-    "${JAVA}" "${TYPE}" -cp "${CLASSPATH}" clojure.main jvm-ptr-size.clj
+    "${JAVA}" "${TYPE}" -cp "${PS_FULL_CLJ_CLASSPATH}" clojure.main jvm-ptr-size.clj sysinfo
+    "${JAVA}" "${TYPE}" -cp "${PS_FULL_CLJ_CLASSPATH}" clojure.main jvm-ptr-size.clj
 
     set +x
     for j in 2 4 8 16 32 64 128 256 384 512 640 768 896 1024 1152 1280 1408 1536 1664 1792 1920
     do
 	echo "${JAVA} "${TYPE}" -Xmx${j}m"
-	"${JAVA}" "${TYPE}" -Xmx${j}m -cp "${CLASSPATH}" clojure.main jvm-ptr-size.clj 
+	"${JAVA}" "${TYPE}" -Xmx${j}m -cp "${PS_FULL_CLJ_CLASSPATH}" clojure.main jvm-ptr-size.clj 
     done
 done
