fb145b857db5e251ede6c23599e0067207a074a7
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2010-11-28 19:03:47 -0800

    Add a simple shell script to run measureproc on the Clojure version of the fannkuchredux program.

---
+++ fannkuchredux/measure-clj-long.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ $# -lt 1 ]
+then
+	1>&2 echo "usage: `basename $0` <clj-version>"
+	exit 1
+fi
+CLJ_VERSION="$1"
+shift
+source ../env.sh
+
+set -x
+../bin/measureproc --jvm-gc-stats hotspot --output output/long-${CLJ_VERSION}-output.txt "${JAVA}" -server -cp "${PS_FULL_CLJ_CLASSPATH}" fannkuchredux 12
