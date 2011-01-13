45f9d0b0c6e309ffd17c5c5ae8e167a6f504da3c
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-01-12 20:58:10 -0800

    Add problems for which new Clojure programs have been developed in the last few months to compile-all-clj.sh script.

--- bin/compile-all-clj.sh
+++ bin/compile-all-clj.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for j in fannkuch fannkuchredux fasta knuc mandelbrot n-body rcomp regex-dna rlines
+for j in binarytrees fannkuch fannkuchredux fasta knuc mandelbrot n-body rcomp regex-dna rlines spectralnorm
 do
     for CLJ_VERSION in clj-1.2 clj-1.3-alpha1 clj-1.3-alpha3 clj-1.3-alpha4
     do
