6a5795cfd102684e85fbe0b5b4cd04b135247c1e
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-01-21 10:05:13 -0800

    Update so that it works on Windows XP + Cygwin as well as Mac OS X and Linux, and for the JRockit JVM as well as Hotspot.

--- collatz/clj-run.sh
+++ collatz/clj-run.sh
@@ -12,4 +12,20 @@ source ../env.sh
 
 mkdir -p output
 
-../bin/measureproc --jvm-info server --jvm-gc-stats hotspot --output output/${CLJ_VERSION}-output.txt "${JAVA}" -server -Xmx64m -classpath "${PS_FULL_CLJ_CLASSPATH}" collatz "$@"
+# If Hotspot JVM is in use:
+JVM_TYPE=hotspot
+MAX_HEAP_MB=64
+# If JRockit JVM is in use:
+#JVM_TYPE=jrockit
+#MAX_HEAP_MB=512
+
+# TBD: Does measureproc need this on Cygwin in order to work?  Is
+# there some straightforward way to make measureproc detect when this
+# change is needed on its own and do it automatically, but only when
+# needed?
+if [ "$OS" == "Cygwin" ]
+then
+    JAVA="`cygpath -w "${JAVA}"`"
+fi
+
+../bin/measureproc --jvm-info server --jvm-gc-stats "${JVM_TYPE}" --output output/${CLJ_VERSION}-output.txt "${JAVA}" -server -Xmx${MAX_HEAP_MB}m -classpath "${PS_FULL_CLJ_CLASSPATH}" collatz "$@"
