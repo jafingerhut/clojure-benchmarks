9a4fb27174b0c97fd0ec5ed494d2e0dcea128086
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-01-26 11:51:14 -0800

    Tweak pidigits/gcc-compile.sh so it works on Mac OS X.

--- pidigits/gcc-compile.sh
+++ pidigits/gcc-compile.sh
@@ -4,5 +4,22 @@ source ../env.sh
 
 gcc --version
 mkdir -p "${GCC_OBJ_DIR}"
-gcc -pipe -Wall -O3 -fomit-frame-pointer -march=native -lgmp pidigits.gcc-4.c -o "${GCC_OBJ_DIR}/pidigits.gcc-4.gcc_run"
+
+if [ "$OS" == "Darwin" ]
+then
+    # -march=native causes problems for Apple's gcc
+    # If you use MacPorts to install the gmp package, then its header
+    # and compiled library files will be in the indicated directories.
+    if [ -r /opt/local/include/gmp.h ]
+    then
+	CFLAGS="-I/opt/local/include -L/opt/local/lib -pipe -Wall -O3 -fomit-frame-pointer -lgmp"
+    else
+	1>&2 echo "On Mac OS X, one way to get the required gmp library is by installing MacPorts and then 'sudo port install gmp'.  Or if you've installed it some other way, modify script $0 to point at it."
+	exit 1
+    fi
+else
+    CFLAGS="-pipe -Wall -O3 -fomit-frame-pointer -march=native -lgmp"
+fi
+
+gcc $CFLAGS pidigits.gcc-4.c -o "${GCC_OBJ_DIR}/pidigits.gcc-4.gcc_run"
 
