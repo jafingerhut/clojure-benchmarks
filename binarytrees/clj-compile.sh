0b8b6d819ba12f68690de2d10c15cca728403ae9
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-01-27 12:03:45 -0800

    Make small modifications to binarytrees.clj-3.clj so that it compiles and uses desired primitives on both Clojure 1.2 and 1.3alpha*

--- binarytrees/clj-compile.sh
+++ binarytrees/clj-compile.sh
@@ -12,6 +12,6 @@ source ../env.sh
 "${RM}" -fr "${CLJ_OBJ_DIR}"
 mkdir -p "${CLJ_OBJ_DIR}"
 
-"${CP}" binarytrees.clj-3.clj "${CLJ_OBJ_DIR}/binarytrees.clj"
+"${CP}" binarytrees.clj-3-alpha.clj "${CLJ_OBJ_DIR}/binarytrees.clj"
 
 "${JAVA}" "-Dclojure.compile.path=${PS_CLJ_OBJ_DIR}" -classpath "${PS_FULL_CLJ_CLASSPATH}" clojure.lang.Compile binarytrees
