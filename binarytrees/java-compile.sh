a85c717b463d6440380b94717027429ee388763a
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-02-10 09:43:27 -0800

    Update java-compile.sh to compile the fastest Java program on the web site.

--- binarytrees/java-compile.sh
+++ binarytrees/java-compile.sh
@@ -5,7 +5,7 @@ source ../env.sh
 "${JAVA}" -version
 "${JAVAC}" -version
 # No need to rename this file
-#"${CP}" binarytrees.java binarytrees.java
+#"${CP}" binarytrees.java-2.java binarytrees.java
 mkdir -p "${JAVA_OBJ_DIR}"
 "${JAVAC}" -d "${JAVA_OBJ_DIR}" binarytrees.java
 #"${RM}" binarytrees.java
