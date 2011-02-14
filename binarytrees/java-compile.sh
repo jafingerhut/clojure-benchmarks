6591d6411172d7d46b9ba31879ae74d3a48d3269
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-02-14 13:33:04 -0800

    Correct binarytrees Java compile script.

--- binarytrees/java-compile.sh
+++ binarytrees/java-compile.sh
@@ -4,8 +4,7 @@ source ../env.sh
 
 "${JAVA}" -version
 "${JAVAC}" -version
-# No need to rename this file
-#"${CP}" binarytrees.java-2.java binarytrees.java
+"${CP}" binarytrees.java-2.java binarytrees.java
 mkdir -p "${JAVA_OBJ_DIR}"
 "${JAVAC}" -d "${JAVA_OBJ_DIR}" binarytrees.java
-#"${RM}" binarytrees.java
+"${RM}" binarytrees.java
