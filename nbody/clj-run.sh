61e1a05e413bc02b3cb73f633fdc985a43b10e22
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-02-14 21:40:41 -0800

    Update clj-run.sh scripts for JRockit JVM, which must have max heap at least 16 MB
    or it will not run.

--- nbody/clj-run.sh
+++ nbody/clj-run.sh
@@ -18,6 +18,12 @@ shift
 # Use a small limit to avoid using lots of memory.  It makes the
 # garbage collector collect more often, but the extra CPU time is not
 # much.
-MAX_HEAP_MB=8
+if [ "${JVM_TYPE}" == "jrockit" ]
+then
+    # JRockit cannot go below max heap size of 16 MB it seems.
+    MAX_HEAP_MB=16
+else
+    MAX_HEAP_MB=8
+fi
 
 ../bin/measureproc ${MEASUREPROC_SHOWS_JVM_INFO} --jvm-gc-stats "${JVM_TYPE}" --output "${OUTP}" "${JAVA}" ${JAVA_PROFILING} -server -Xmx${MAX_HEAP_MB}m -classpath "${PS_FULL_CLJ_CLASSPATH}" nbody "$@"
