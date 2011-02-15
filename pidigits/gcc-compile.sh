ca708caa78c43f7c17d2259769ed4c14c57b4d34
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-02-14 21:42:08 -0800

    Update pidigits GCC compile script for Cygwin GCC.

--- pidigits/gcc-compile.sh
+++ pidigits/gcc-compile.sh
@@ -17,9 +17,12 @@ then
 	1>&2 echo "On Mac OS X, one way to get the required gmp library is by installing MacPorts and then 'sudo port install gmp'.  Or if you've installed it some other way, modify script $0 to point at it."
 	exit 1
     fi
+elif [ "$OS" == "Cygwin" ]
+then
+    CFLAGS="-Wall -O3"
+    LDFLAGS="-lgmp"
 else
     CFLAGS="-pipe -Wall -O3 -fomit-frame-pointer -march=native -lgmp"
 fi
 
-gcc $CFLAGS pidigits.gcc-4.c -o "${GCC_OBJ_DIR}/pidigits.gcc-4.gcc_run"
-
+gcc $CFLAGS pidigits.gcc-4.c $LDFLAGS -o "${GCC_OBJ_DIR}/pidigits.gcc-4.gcc_run"
