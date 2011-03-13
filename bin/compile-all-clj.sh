013e99d7d0ae81092094d458c6c974cad96e1c0f
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-03-13 11:34:25 -0700

    Tweak compile-all-clj.sh script so one can specify list of Clojure versions on cmd line

--- bin/compile-all-clj.sh
+++ bin/compile-all-clj.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
+if [ $# -lt 1 ]
+then
+    CLJ_VERSIONS="clj-1.2 clj-1.3-alpha1 clj-1.3-alpha3 clj-1.3-alpha4 clj-1.3-alpha4 clj-1.3-alpha6"
+else
+    CLJ_VERSIONS="$@"
+fi
+
 for j in binarytrees fannkuch fannkuchredux fasta knucleotide mandelbrot nbody regexdna revcomp revlines spectralnorm
 do
-    for CLJ_VERSION in clj-1.2 clj-1.3-alpha1 clj-1.3-alpha3 clj-1.3-alpha4
+    for CLJ_VERSION in ${CLJ_VERSIONS}
     do
         cd $j
         \rm -fr obj/${CLJ_VERSION}
