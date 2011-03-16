5f937f58cd26855fa395b76dbe15f84940db0845
John Andrew Fingerhut <andy_fingerhut@alum.wustl.edu>
2011-03-15 23:01:32 -0700

    Remove obsolete fannkuch problem from default list run by run-all.sh script

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
-for j in binarytrees fannkuch fannkuchredux fasta knucleotide mandelbrot nbody regexdna revcomp spectralnorm
+# fannkuch works for Java and Clojure, but it is one of the slowest
+# Clojure programs, partly because it has received less optimization
+# attention because it is now considered obsolete on the Computer
+# Language Benchmarks Game web site.  Leave it out unless someone
+# really wants it by editing this file.
+
+# pidigits works for Clojure version that does not use the GMP
+# library, but the Java and C versions requires the GMP library.
+# Leave it out by default.
+
+# chameneosredux and threadring have not yet been implemented in
+# Clojure.
+
+#for j in binarytrees fannkuch fannkuchredux fasta knucleotide mandelbrot meteor nbody pidigits regexdna revcomp spectralnorm
+
+for j in binarytrees fannkuchredux fasta knucleotide mandelbrot meteor nbody regexdna revcomp spectralnorm
 do
     cd $j
     ./batch.sh "$@"
