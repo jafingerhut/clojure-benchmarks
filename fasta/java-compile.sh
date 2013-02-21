6d778a662392ed334c7690d1bce3bcdcde9e4701
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-02-20 18:20:40 -0800

    Update Clojure and Java programs to use for fasta benchmark
    Made them match the current fastest ones for the 4-core 64-bit
    hardware used by the Computer Language Benchmarks Game web site as of
    Feb 20 2013.

--- fasta/java-compile.sh
+++ fasta/java-compile.sh
@@ -4,7 +4,7 @@ source ../env.sh
 
 "${JAVA}" -version
 "${JAVAC}" -version
-"${CP}" fasta.java-3.java fasta.java
+"${CP}" fasta.java-4.java fasta.java
 mkdir -p "${JAVA_OBJ_DIR}"
 "${JAVAC}" -d "${JAVA_OBJ_DIR}" fasta.java
 "${RM}" fasta.java
