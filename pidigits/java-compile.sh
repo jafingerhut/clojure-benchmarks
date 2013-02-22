f93787e061739b041d9370310db80d3853f095d7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-02-21 16:39:53 -0800

    Update Java pidigits program to fastest from Benchmarks Game site as of Feb 21 2013 for 4 core 64-bit hardware

--- pidigits/java-compile.sh
+++ pidigits/java-compile.sh
@@ -4,7 +4,5 @@ source ../env.sh
 
 "${JAVA}" -version
 "${JAVAC}" -version
-"${CP}" pidigits.java-4.java pidigits.java
 mkdir -p "${JAVA_OBJ_DIR}"
 "${JAVAC}" -d "${JAVA_OBJ_DIR}" pidigits.java
-"${RM}" pidigits.java
