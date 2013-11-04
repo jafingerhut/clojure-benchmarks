97504d6ffea8c827a7d7b5ae6bce9f7aa716c39f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-11-04 10:06:44 -0800

    Update cljexprs output file names for VMware Fusion and Ubuntu 12.04.3

--- cljexprs/scripts/runseveral.sh
+++ cljexprs/scripts/runseveral.sh
@@ -11,19 +11,19 @@ do
 	case $jdk in
 	    1)
 		source ${HOME}/jdks/setup-oracle-1.6-32bit.sh
-		SAVEDIR="./results/andys-macpro-osx-10.6.8-virtualbox-4.2.4-ubuntu-12.04.1-oracle-jdk1.6-32bit"
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.6-32bit"
 		;;
 	    2)
 		source ${HOME}/jdks/setup-oracle-1.6-64bit.sh
-		SAVEDIR="./results/andys-macpro-osx-10.6.8-virtualbox-4.2.4-ubuntu-12.04.1-oracle-jdk1.6-64bit"
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.6-64bit"
 		;;
 	    3)
 		source ${HOME}/jdks/setup-oracle-1.7-32bit.sh
-		SAVEDIR="./results/andys-macpro-osx-10.6.8-virtualbox-4.2.4-ubuntu-12.04.1-oracle-jdk1.7-32bit"
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.7-32bit"
 		;;
 	    4)
 		source ${HOME}/jdks/setup-oracle-1.7-64bit.sh
-		SAVEDIR="./results/andys-macpro-osx-10.6.8-virtualbox-4.2.4-ubuntu-12.04.1-oracle-jdk1.7-64bit"
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.7-64bit"
 		;;
 	    5)
 		# No need to source any setup file on Mac OS X
