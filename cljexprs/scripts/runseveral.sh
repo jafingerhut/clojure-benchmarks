83c31779e935a044346a9de53b7b6bf99c2b3c8b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-04-07 10:01:41 -0700

    Add JDK 1.8 to runseveral.sh script for cljexprs on Linux

--- cljexprs/scripts/runseveral.sh
+++ cljexprs/scripts/runseveral.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 set -x
-# 0-4 for Linux.  0 must be first unless I make a setup script for OpenJDK 1.6
-#for jdk in 0 1 2 3 4
-# 5 for Mac OS X
-for jdk in 5
+# 0-6 for Linux.  0 must be first unless I make a setup script for OpenJDK 1.6
+#for jdk in 0 1 2 3 4 5 6
+# 100 for Mac OS X
+for jdk in 100
 do
     for run in 1 2 3
     do
@@ -30,6 +30,14 @@ do
 		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.7-64bit"
 		;;
 	    5)
+		source ${HOME}/jdks/setup-oracle-1.8-32bit.sh
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.8-32bit"
+		;;
+	    6)
+		source ${HOME}/jdks/setup-oracle-1.8-64bit.sh
+		SAVEDIR="./results/andys-macpro-osx-10.6.8-vmwarefusion-3.1.4-ubuntu-12.04.3-oracle-jdk1.8-64bit"
+		;;
+	    100)
 		# No need to source any setup file on Mac OS X
 		SAVEDIR="./results/andys-macpro-osx-10.6.8-oracle-jdk1.6-64bit"
 		;;
